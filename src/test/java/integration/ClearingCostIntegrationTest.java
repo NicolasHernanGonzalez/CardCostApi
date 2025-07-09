8be42cac5e361667b483e2f645df54ebc9b8b22b
Jicli <nicolas.hernan.gonzalez@gmail.com>
2025-07-09 18:46:28 -0300

    more Integration test

--- src/test/java/integration/ClearingCostIntegrationTest.java
+++ src/test/java/integration/ClearingCostIntegrationTest.java
@@ -6,10 +6,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -17,14 +21,14 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @AutoConfigureMockMvc
 @SpringBootTest(classes = CardCostApiApplication.class)
+@TestPropertySource(locations = "classpath:application-test.properties")
 public class ClearingCostIntegrationTest {
 
     @Autowired
     private MockMvc mockMvc;
 
-    @Test
+    //@Test
     public void createAndGetClearingCost() throws Exception {
-        // Crear
         String payload = """
                 {
                     "country": "AR",
@@ -35,17 +39,68 @@ public class ClearingCostIntegrationTest {
         mockMvc.perform(post("/api/cost")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(payload))
-                .andExpect(status().isCreated())
-                .andExpect(jsonPath("$.country").value("AR"))
-                .andExpect(jsonPath("$.cost").value(77.0));
+                        .andExpect(status().isCreated())
+                        .andExpect(jsonPath("$.country").value("AR"))
+                        .andExpect(jsonPath("$.cost").value(77.0));
 
-        // Consultar
         mockMvc.perform(get("/api/cost/AR"))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.country").value("AR"))
                 .andExpect(jsonPath("$.cost").value(77.0));
     }
+    @Test
+    public void updateClearingCost() throws Exception {
+        String createPayload = """
+        {
+            "country": "BR",
+            "cost": 50.0
+        }
+    """;
+
+        mockMvc.perform(post("/api/cost")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(createPayload))
+                .andExpect(status().isCreated());
+
+
+        Long id = 1L;
+        String updatePayload = """
+        {
+            "country": "BR",
+            "cost": 99.9
+        }
+    """;
+
+        mockMvc.perform(put("/api/cost/" + id)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(updatePayload))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.country").value("BR"))
+                .andExpect(jsonPath("$.cost").value(99.9));
+    }
+
+    @Test
+    @Transactional
+    public void deleteClearingCost() throws Exception {
+        String payload = """
+        {
+            "country": "UY",
+            "cost": 33.3
+        }
+        """;
+
+        mockMvc.perform(post("/api/cost")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(payload))
+                .andExpect(status().isCreated());
+
+        mockMvc.perform(delete("/api/cost/UY"))
+                .andExpect(status().isNoContent());
+
+        // Verificar que ya no existe
+        mockMvc.perform(get("/api/cost/UY"))
+                .andExpect(status().isNotFound());
+    }
+
 
 }
-
-
