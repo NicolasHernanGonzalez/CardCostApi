b4d74930a02da53ec593cf524cb4de49292c137d
Jicli <nicolas.hernan.gonzalez@gmail.com>
2025-07-10 10:27:35 -0300

    min config changes

--- src/main/java/com/cardcostapi/config/InMemoryRateLimitConfig.java
+++ src/main/java/com/cardcostapi/config/InMemoryRateLimitConfig.java
@@ -5,8 +5,6 @@ import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
-import java.time.Duration;
-
 @Component
 @ConfigurationProperties(prefix = "ratelimit")
 @Getter
